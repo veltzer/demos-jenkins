392a8b595833d7804136808f831ebee0ad6b6f48
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 01:52:19 +0200

    new pydmt version

--- config/python.py
+++ config/python.py
@@ -1,7 +1,10 @@
-config_requires = []
-dev_requires = []
-install_requires = []
-make_requires = [
+from typing import List
+
+
+config_requires: List[str] = []
+dev_requires: List[str] = []
+install_requires: List[str] = []
+make_requires: List[str] = [
     "pymakehelper",
     "pydmt",
     "pylint",
@@ -13,5 +16,5 @@ make_requires = [
     "types-chardet",
     "types-PyYAML",
 ]
-test_requires = []
+test_requires: List[str] = []
 requires = config_requires + install_requires + make_requires + test_requires

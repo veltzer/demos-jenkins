4b4660e837834c87506cdb9899e8f8aae6a1080d
Mark Veltzer <mark.veltzer@gmail.com>
2024-06-11 13:06:23 +0300

    no commit message given

--- exercises/basic/13_java_and_maven/src/main/java/com/company/HelloWorld.java
+++ exercises/basic/13_java_and_maven/src/main/java/com/company/HelloWorld.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        String text = "This is a simple Maven app using Apache Commons Lang.";
+        String reversed = StringUtils.reverse(text);
 
-  public static void main(String[] args) {
-    System.out.println("Hello World!"); 
-  }
-
+        System.out.println("Original: " + text);
+        System.out.println("Reversed: " + reversed);
+    }
 }
